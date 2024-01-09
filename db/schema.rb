b6dc5ab798155dabdb48fb82defa008489658b09
Sean Warren <seanwarren77@gmail.com>
2024-01-09 11:41:05 -0500

    add customer model
    bin/rails g scaffold Customer name:string phone:string email:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_09_144844) do
+ActiveRecord::Schema.define(version: 2024_01_09_163647) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "trucks", force: :cascade do |t|
     t.string "name"
