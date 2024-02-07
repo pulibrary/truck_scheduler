5cf2c160edd3f265bae1d150b01f686c19a862cc
Sean Warren <seanwarren77@gmail.com>
2024-02-07 11:48:43 -0500

    add validation on reservation end time
    ref #19
    
    this commit uses a feature introduced in rails 7, test currently fails
    
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :customer
   belongs_to :truck
+  
+  validates :end_time, comparison: { greater_than: :start_time }
 end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#end_time" do
+    it "is after the start time" do
+      reservation = Reservation.create(
+        customer: Customer.create,
+        truck: Truck.create,
+        start_time: DateTime.now,
+        end_time: DateTime.now - 1.day
+      )
+    
+      expect(reservation).to be_invalid
+    end
+  end
 end
+
+
+
+
